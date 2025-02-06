6adfddcc8a3a678d180ec84085ad3fc71df8a198
JeffersonDRR <jefferson9908@hotmail.com>
2025-02-06 11:52:09 -0500

    Parte 27

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,13 +1,13 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then((registration) => {
-          console.log('Service Worker registrado con éxito:', registration);
-        })
-        .catch((error) => {
-          console.log('Error al registrar el Service Worker:', error);
-        });
-    });
-  }
-  
-  console.log('PWA funcionando');
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch((error) => {
+        console.log('Error al registrar el Service Worker:', error);
+      });
+  });
+}
+
+console.log('PWA funcionando');
