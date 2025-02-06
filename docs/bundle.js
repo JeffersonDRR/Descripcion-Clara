d211341a5d1b0aaad9205b786b43955da76ca887
JeffersonDRR <jefferson9908@hotmail.com>
2025-02-06 12:27:33 -0500

    Parte 33

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,8 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/DescripcionClara.Web/docs/service-worker.js', {
-      scope: '/DescripcionClara.Web/docs/'
-    })
+    navigator.serviceWorker.register('./service-worker.js')
       .then((registration) => {
         console.log('Service Worker registrado con éxito:', registration);
       })
