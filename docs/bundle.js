460814c952549e415e8db6398af18b255ebee8cf
JeffersonDRR <jefferson9908@hotmail.com>
2025-02-06 12:33:59 -0500

    Parte 35

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,6 +1,8 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
+    navigator.serviceWorker.register('/DescripcionClara.Web/docs/service-worker.js', {
+      scope: '/DescripcionClara.Web/docs/'
+    })
       .then((registration) => {
         console.log('Service Worker registrado con éxito:', registration);
       })
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,12 +1,14 @@
 const CACHE_NAME = 'descripcion-clara-cache';
+const BASE_PATH = '/DescripcionClara.Web/docs/';
 const urlsToCache = [
-  './',
-  './index.html',
-  './styles.css',
-  './index.js',
-  './icon-192x192.png',
-  './icon-512x512.png',
-  './manifest.json'
+  BASE_PATH,
+  BASE_PATH + 'index.html',
+  BASE_PATH + 'styles.css',
+  BASE_PATH + 'index.js',
+  BASE_PATH + 'bundle.js',
+  BASE_PATH + 'icon-192x192.png',
+  BASE_PATH + 'icon-512x512.png',
+  BASE_PATH + 'manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
